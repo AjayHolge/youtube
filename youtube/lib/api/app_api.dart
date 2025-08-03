7ed8c4047b204068a76426e68c49bf52811832b0
Ajay Holge <ajayholge@example.com>
2025-08-03 14:58:38 +0530

    Initial commit

--- youtube/lib/api/app_api.dart
+++
@@ -1,6 +0,0 @@
-class BaseUrl {
-  static const String baseurl = "https://krushimahostav.yuvapsvs.com";
-
-  static const String loginUrl =
-      "https://krushimahostav.yuvapsvs.com/api/auth/login";
-}
